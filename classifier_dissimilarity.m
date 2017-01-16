7ad42681e2c8cf625a2966f574ffc0973b983f3d
Reza Permadi <reditya@gmail.com>
2017-01-16 16:23:38 +0100

    push dissimilarity

--- classifier_dissimilarity.m
+++ classifier_dissimilarity.m
@@ -1,10 +1,16 @@
 % dissimilarity representation
-w = scalem([],'variance')*proxm([]);
-clsf_dis_knnc = {w*knnc([],3)};
-clsf_dis_fisherc = {w*fisherc([])};
+% w = scalem([],'variance')*proxm([],'c');
+%w = proxm([],'c');
+%clsf_dis_knnc = {w*knnc([],3)};
+%clsf_dis_fisherc = {w*fisherc([])};
 %e0 = clevalf(trn_set,classifier,featnum,[],1,tst_set);
-% [E_knn_f_d,Std_knn_f_d] = prcrossval(dataset_pixel, clsf_dis, 10, 2);
-[E_fisher_p_d,Std_fisher_p_d] = prcrossval(dataset_pixel, clsf_dis_fisherc, 10, 2);
+%[E_knn_f_d,Std_knn_f_d] = prcrossval(dataset_pixel, clsf_dis_knnc, 10, 2);
+% [E_fisher_p_d,Std_fisher_p_d] = prcrossval(dataset_pixel, clsf_dis_fisherc, 10, 2);
+
+small_reps = gendat(dataset_pixel, 0.05);
+w = proxm(small_reps,'c');
+dataset_pixel_dis = dataset_pixel*w;
+%scatterd(c*pca(c,2));
 
 
 
--- pca.m
+++ pca.m
@@ -1,14 +1,16 @@
 % using pca and evaluate with clevalf
 featnum = [1:1:256];
-res_qdc = zeros(size(featnum));
+% featnum = [1:10:250];
+res_knnc = zeros(size(featnum));
+% res_dis = zeros(size(featnum));
 for c = featnum
-    classifier = ldc([]);
+    classifier = knnc([], 3);
     %classifier = svc([],proxm('r',2));
     %[trn,tst] = gendat(dataset_pixel,0.5);
     wp = pcam([],c);
     %clsf = knnc(trn,3)*wp;
     %res(c) = testc(tst,clsf);
-    res_ldc(c) = prcrossval(dataset_pixel,wp*classifier,10,1);
+    res_knnc(c) = prcrossval(dataset_pixel,wp*classifier,10,1);
 end
 %featnum = [1:1:256];
 %Ecleval_2 = clevalf(dataset_pixel,wp*classifier,featnum,[],1);
