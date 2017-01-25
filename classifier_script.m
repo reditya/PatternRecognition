810d31262277575b6e19f5d1944bb2cfffce3145
Reza Permadi <reditya@gmail.com>
2017-01-25 20:22:19 +0100

    some stuff

--- classifier_script.m
+++ classifier_script.m
@@ -30,3 +30,12 @@ classifier_names = {'knnc2', 'knnc3', 'knnc4', 'knnc5', 'qdc', 'ldc', 'parzenc',
 %[E_knn_profiles,Std_knn_profiles] = prcrossval(dataset_profiles, scale_factor*classifier, 20, 2);
 %[E_knn_features,Std_knn_features] = prcrossval(dataset_features, classifier, 10, 2);
 running_time = toc;
+
+%%%% DESKEWED VERSION EVALUATION %%%%
+
+%%% SCENARIO 1 %%%
+tic;
+classifier = {knnc([],2), knnc([],3), knnc([],4), knnc([],5), qdc([]), ldc([]), parzenc([]), loglc([]), nmc([]), fisherc([])};
+classifier_names = {'knnc2', 'knnc3', 'knnc4', 'knnc5', 'qdc', 'ldc', 'parzenc', 'loglc', 'nmc', 'fisherc'};
+[E_pixels_deskewed,Std_pixels_deskewed] = prcrossval(dataset_deskew, classifier, 10, 2);
+running_time = toc;
