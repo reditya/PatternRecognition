d886ac334b8c5e3e6ae7f31cfa4558225ac33e74
Ruben Vroegindeweij <ruben_vroegindeweij@hotmail.com>
2017-01-30 21:38:00 +0100

    Optimal classifiers added to evaluation nist_eval

--- evaluation_nist_eval.m
+++ evaluation_nist_eval.m
@@ -6,9 +6,9 @@
 
 % Scenario 1
 % Best classifier is : PCA on 40, combining [knn3 parzen qd], combine on median 
-we1_1 = pcam(dataset_pixel_basic,40)*knnc([],3);
-we2_1 = pcam(dataset_pixel_basic,32)*parzenc([],1.20);  % h is from optimizing parzenc using built-in function in parzenc.m
-we3_1 = pcam(dataset_pixel_basic,45)*qdc([]);
+we1_1 = pcam(dataset_pixel_basic,34)*knnc([],3);
+we2_1 = pcam(dataset_pixel_basic,34)*parzenc([],1.20);  % h is from optimizing parzenc using built-in function in parzenc.m
+we3_1 = pcam(dataset_pixel_basic,34)*qdc([]);
 combined_we_1 = [we1_1 we2_1 we3_1];
 clsf_nist_1 = dataset_pixel_basic*combined_we_1*medianc;
 
@@ -17,8 +17,8 @@ err_scenario_1 = nist_eval('preprocess_basic',clsf_nist_1,100);
 % Scenario 2
 % For scenario 2: pca 22 for parzen and ld and 21 for fisher and combining
 % on prodc
-we1_2 = pcam(dataset_deskew_small,22)*parzenc([],0.78); % h is from optimizing parzenc using built-in function in parzenc.m
-we2_2 = pcam(dataset_deskew_small,22)*ldc([]);
+we1_2 = pcam(dataset_deskew_small,21)*parzenc([],0.78); % h is from optimizing parzenc using built-in function in parzenc.m
+we2_2 = pcam(dataset_deskew_small,21)*ldc([]);
 we3_2 = pcam(dataset_deskew_small,21)*fisherc([]);
 combined_we_2 = [we1_2 we2_2 we3_2];
 clsf_nist_2 = dataset_deskew_small*combined_we_2*prodc;
